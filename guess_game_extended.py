8bc86d582bd64e42f587ef21d04f1772ce0e030c
Audrius Metrikis <audriusmetrikis@gmail.com>
2025-05-18 11:11:27 +0300

    Add number guessing game and age calculator

---
+++ guess_game_extended.py
@@ -0,0 +1,38 @@
+# This will be a simple number guessing game
+# The user will have tu guess a number between 1 and 10
+# if the user guess number is incorrect , the program will tell
+# either "to high" "too low"   
+
+# Problems:
+# Does not handle non-integer inputs
+# Does not handle out of range inputs
+# Does not handle multiple guesses
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 10")
+
+import random
+
+number_to_guess = random.randint(1, 10)
+
+user_input = input("Please enter your guess: ")
+
+print("You guessed: " + user_input)
+print(f"You guessed: {user_input}")
+
+try:
+    parsed_guess = int(user_input)
+except Exception:  
+    print("Invalid input! Please enter a valid integer.")
+    exit()
+if parsed_guess < 1 or parsed_guess > 10:
+    print("Your guess is out of range! Please guess a number between 1 and 10.python guess_game_extended.py") 
+
+elif parsed_guess < number_to_guess:
+    print("Too low")
+elif parsed_guess > number_to_guess:
+    print("Too high")
+else:
+    print("Congralutions! You guessed the number correctly")
+
+print("The number was: " + str(number_to_guess))
